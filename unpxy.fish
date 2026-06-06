f5225a04fcec574f59d8e16eb9bf087777201844
0xA672 <258348964+0xA672@users.noreply.github.com>
2026-06-06 09:54:43 +0800

    Refactor unpxy function to handle proxy variables

--- unpxy.fish
+++ unpxy.fish
@@ -1,9 +1,20 @@
 function unpxy --description 'Run a command without proxy environment variables'
-    begin
-        set -l _vars http_proxy https_proxy HTTP_PROXY HTTPS_PROXY all_proxy ALL_PROXY no_proxy NO_PROXY
-        for v in $_vars
-            set -e $v
+    set -l proxy_vars http_proxy https_proxy HTTP_PROXY HTTPS_PROXY all_proxy ALL_PROXY no_proxy NO_PROXY
+
+    if test (count $argv) -gt 0 -a "$argv[1]" = "-p"
+        set -e argv[1] 
+        begin
+            for v in $proxy_vars
+                set -e $v
+            end
+            eval $argv
         end
-        command $argv
+    else
+        begin
+            for v in $proxy_vars
+                set -e $v
+            end
+            command $argv
+        end
     end
 end
