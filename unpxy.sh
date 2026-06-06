22909ea778b4e55fcf9158505b90e992fbfe7b01
0xA672 <258348964+0xA672@users.noreply.github.com>
2026-06-06 09:52:53 +0800

    Refactor unpxy function to handle '-p' option

--- unpxy.sh
+++ unpxy.sh
@@ -1,7 +1,14 @@
 unpxy() {
-  (
-    unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY \
-          all_proxy ALL_PROXY no_proxy NO_PROXY
-    exec "$@"
-  )
+    if [[ "$1" == "-p" ]]; then
+        shift
+        (
+            unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY all_proxy ALL_PROXY no_proxy NO_PROXY
+            eval "$@"
+        )
+    else
+        (
+            unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY all_proxy ALL_PROXY no_proxy NO_PROXY
+            exec "$@"
+        )
+    fi
 }
